2116822cf22d29a470674b87c30b14eb230f1b25
AnguiDev <angiedesarrollo@gmail.com>
2021-04-27 17:59:39 -0500

    print last digit negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = ((number % 10)) #last digit#
+if number < 0:
+	last_digit = ((-number % 10)*-1)
+else:
+	last_digit = ((number % 10)) #last digit#
  #follow last digit of number 
 if last_digit > 5:
 	 print("Last digit of, {:d} is {:d} and is greater than 5".format(number, (last_digit)))
 if last_digit == 0:
 	print("Last digit of, {:d} is {:d} and is 0".format(number, (last_digit)))
-elif ((last_digit < 6)*-1) and number != 0:
+elif (last_digit < 6) and number != 0:
 	print("Last digit of, {:d} is {:d} and is less than 6 and not 0".format(number, (last_digit)))
-
-#number * -1
-#if (number % 10) > 5:
-#    print("Last digit of, {:d} is {:d} and is greater than 5".format(number, (number % 10)))
-#elif (number % 10) < 6 and  (number * -1) != 0:
-#   print("Last digit of, {:d} is {:d} and is less than 6 and not 0".format(number, (number % 10)))
-#else: 
-#    print("Last digit of, {:d} is {:d} and is 0".format(number, (number % 10)))
- 
