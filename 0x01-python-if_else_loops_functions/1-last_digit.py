83b68b2bdd28097e7a2de2c92ebd598ef9fed779
AnguiDev <angiedesarrollo@gmail.com>
2021-04-27 15:09:18 -0500

    print last digit of number random

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,10 @@ last_digit = ((number % 10)) #last digit#
  #follow last digit of number 
 if last_digit > 5:
 	 print("Last digit of, {:d} is {:d} and is greater than 5".format(number, (last_digit)))
-elif last_digit == 0:
+if last_digit == 0:
 	print("Last digit of, {:d} is {:d} and is 0".format(number, (last_digit)))
-
-if last_digit < 6 and number != 0:
-	print("Last digit of, {:d} is {:d} and is less than 6 and not 0".format(number, (last_digit * -1)))
+elif ((last_digit < 6)*-1) and number != 0:
+	print("Last digit of, {:d} is {:d} and is less than 6 and not 0".format(number, (last_digit)))
 
 #number * -1
 #if (number % 10) > 5:
