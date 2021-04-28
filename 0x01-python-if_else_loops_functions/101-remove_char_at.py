d7e97ea4d4c51b50c3b4338c5b07db35087cd882
AnguiDev <angiedesarrollo@gmail.com>
2021-04-28 00:30:23 -0500

    advance created copy string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin//python3
+def remove_char_at(str, n):
+    cop = ""
+    iter = 0
+    for char in str:
+        if iter != n:
+            cop += char
+        iter = iter+1
+    return cop
