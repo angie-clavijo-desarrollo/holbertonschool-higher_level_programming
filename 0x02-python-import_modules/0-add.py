0362effbb2362122d149e55974fc12b2b096973b
AnguiDev <angiedesarrollo@gmail.com>
2021-04-28 08:15:10 -0500

    import functions of file diferent

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# import add_0#
+# import function add of file add_0#
 from add_0 import add
 a = 1
 b = 2
-# add_0.add(1, 2)#
-# add(1, 2)##
+# add_0.add(a, b)#
+# add(a, b)##
 print("{:d} + {:d} =" .format(a, b), add(a, b))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# import functions of file calculator, * all#
+from calculator_1 import *
+# value of var#
+a = 10
+b = 5
+print("{:d} + {:d} =" .format(a, b), add(a, b))
+print("{:d} - {:d} =" .format(a, b), sub(a, b))
+print("{:d} * {:d} =" .format(a, b), mul(a, b))
+print("{:d} / {:d} =" .format(a, b), div(a, b))
