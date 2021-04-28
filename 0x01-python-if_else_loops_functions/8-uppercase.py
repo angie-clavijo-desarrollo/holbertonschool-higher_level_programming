2b882d817b73d032390cfe626d95824de9ed9dc5
AnguiDev <angiedesarrollo@gmail.com>
2021-04-28 00:08:35 -0500

    convert string to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        text=ord(str(i))
+        text=ord(str[i])
         if text in range(97, 123):
             text=text-32
-    print("{}".format(chr(str)))
+    print("{}".format(chr(text)), end="")
+    print()
