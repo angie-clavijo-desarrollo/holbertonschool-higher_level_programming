a9b76c250849502c8a888d33a3e67a732f3a1323
AnguiDev <angiedesarrollo@gmail.com>
2021-04-27 21:57:39 -0500

    print number that combinations possibles

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in range (0, 10):
+for i in range(0, 10):
         for j in range(1, 10):
                 if i < j:
                         if j == 1:
                                 print("{:d}{:d}".format(i, j), end="")
-                        else:  
+                        else:
                                 print(", {:d}{:d}".format(i, j), end="")
-print (" ")
+print("")
