4c5e2d0b1ba25d2b9f04d1f5efb77819c9030a70
AnguiDev <angiedesarrollo@gmail.com>
2021-04-28 12:13:56 -0500

    import function add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
 # add_0.add(a, b)#
-print (add(a, b))
-#print("{:d} + {:d} =" .format(a, b), add(a, b))
+# print (add(a, b))
+    print("{:d} + {:d} =" .format(a, b), add(a, b))
