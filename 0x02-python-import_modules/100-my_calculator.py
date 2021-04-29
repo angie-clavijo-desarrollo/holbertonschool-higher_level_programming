5f404330ced12c582c47e93e5e5152e3577db2dc
AnguiDev <angiedesarrollo@gmail.com>
2021-04-28 23:04:28 -0500

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif (add, sub, mul, div) == (add, sub, mul, div):
-       print("{:d} + {:d} =" .format(a, b), add(a, b))
-       print("{:d} - {:d} =" .format(a, b), sub(a, b))
-       print("{:d} * {:d} =" .format(a, b), mul(a, b))
-       print("{:d} / {:d} =" .format(a, b), div(a, b))
-       exit(0)
+        print("{:d} + {:d} =" .format(a, b), add(a, b))
+        print("{:d} - {:d} =" .format(a, b), sub(a, b))
+        print("{:d} * {:d} =" .format(a, b), mul(a, b))
+        print("{:d} / {:d} =" .format(a, b), div(a, b))
+        exit(0)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
