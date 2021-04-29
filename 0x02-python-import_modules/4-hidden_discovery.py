dd7772a535418e3a214484bd47762c67fc592e7b
AnguiDev <angiedesarrollo@gmail.com>
2021-04-28 21:59:23 -0500

    print str that start diferent __

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     for i in dir(hidden_4):
-        if(i[0] != '__'):
+        if(i[0] != str('_')):
             print("{}".format(i))
 
     """file  "hidden_4.pyc", in <module>
