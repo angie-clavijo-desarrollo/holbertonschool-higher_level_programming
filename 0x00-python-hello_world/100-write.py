cd196cddd31e82357848d5d587157dc35f36cdd1
AnguiDev <angiedesarrollo@gmail.com>
2021-04-29 06:47:35 -0500

    print an text, not function frint

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-save_file = open("examplefile.txt", 'r')
-save_file_wc = open("examplefile_wc.txt", 'w')
-for line in save_file:
-    tokens = line.split('')
-    save_file_wc.write(line + str(len(tokens)))
-save_file.write(
+import sys
+save_file = open("examplefile.txt", 'w')
+sys.stderr.write(
     "and that piece of art is useful - Dora Korpar, 2015-10-19"'\n')
-exit(0)
 save_file.close()
-save_file_wc.close()
+exit(1)
