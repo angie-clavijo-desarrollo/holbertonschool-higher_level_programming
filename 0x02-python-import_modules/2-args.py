be7c1803688ed458ad350ab6aeb19d5afa64cd3d
AnguiDev <angiedesarrollo@gmail.com>
2021-04-28 20:23:34 -0500

    count number arguments for entry

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("{:d} argument:".format(len(sys.argv)))
-    else:
+    if len(sys.argv) > 2:
         print("{:d} arguments:".format(len(sys.argv)-1))
         for i in range(1, len(sys.argv)):
             print("{:d}: {:s}" .format(i, sys.argv[i]))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv)-1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}" .format(i, sys.argv[i]))
+    else:
+        print("{:d} arguments.".format(len(sys.argv)-1))
