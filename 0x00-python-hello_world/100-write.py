7a176e6ed8b18b0ea220f94c8939d83c620c473d
AnguiDev <angiedesarrollo@gmail.com>
2021-04-29 06:15:47 -0500

    function write to an file new

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-save_file = open("examplefile.txt", 'w')
 save_file = open("examplefile.txt", 'r')
+save_file_wc = open("examplefile_wc.txt", 'w')
+for line in save_file:
+    tokens = line.split('')
+    save_file_wc.write(line + str(len(tokens)))
 save_file.write(
     "and that piece of art is useful - Dora Korpar, 2015-10-19"'\n')
-save_file.close()
 exit(0)
+save_file.close()
+save_file_wc.close()
