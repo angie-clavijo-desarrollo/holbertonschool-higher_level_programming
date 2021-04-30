17d66e7f370a245dde42cf8819df5f4ef4c5bcc0
AnguiDev <angiedesarrollo@gmail.com>
2021-04-30 07:28:44 -0500

    print fizz if is mod 3 or print buzz if is mod of 5 and fizzbuzz if is mod of 3 and 5

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 100):
-        if i % 3 != 0 or i % 5 != 0:
-            print("{:d} " .format(i), end="")
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz ", end="")
         if i % 3 == 0:
             print("Fizz ", end="")
         if i % 5 == 0:
             print("Buzz ", end="")
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz ", end="")
+        if i % 3 != 0 or i % 5 != 0:
+            print("{:d} " .format(i), end="")
