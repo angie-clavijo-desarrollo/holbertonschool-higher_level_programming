2a822d2d7eb90d278c25e9ebcbc24e68e386c674
AnguiDev <angiedesarrollo@gmail.com>
2021-05-05 21:56:28 -0500

    print number of index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    """if idx is negative"""
-    if idx < 0:
-        return None
     """if idx exist is range"""
     if idx in range(my_list[idx]):
         return(my_list[idx])
+    else:
+        return None
