595894e635aa840ff199a9f887ce40315a6f25b9
AnguiDev <angiedesarrollo@gmail.com>
2021-05-06 00:30:25 -0500

    add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple_a[0] != tuple_a[1]:
+        return (tuple_a[1], tuple_a[1]+tuple_b[1])
+    if tuple_a[0] !=  (tuple_a[0] + tuple_a[0]):
+        return ((tuple_a[0]+tuple_a[0], tuple_a[1]))
+    if tuple_a == tuple_a:
+        return (tuple_a)
