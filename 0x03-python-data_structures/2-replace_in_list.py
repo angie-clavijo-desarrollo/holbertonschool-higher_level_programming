5d62d6d83e47c916164d36aecb4a20ca52b36eb4
AnguiDev <angiedesarrollo@gmail.com>
2021-05-05 21:17:14 -0500

    style pep8

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,8 +4,8 @@ def replace_in_list(my_list, idx, element):
     if idx < (my_list[idx]):
         if idx == idx:
             my_list[idx] = element
-            print (my_list)
+            print(my_list)
     if idx > (my_list[idx]):
         if idx == idx:
             my_list[idx] = element
-            print (my_list)
+            print(my_list)
