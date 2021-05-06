cdf1e84aa860052565daab7b3863bbbb1c125e0a
AnguiDev <angiedesarrollo@gmail.com>
2021-05-05 22:06:20 -0500

    replace index that on new element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-
-    if idx < 0:
+    if idx <= 0:
         return my_list
     if idx > (my_list[idx]):
             my_list[idx] = element
