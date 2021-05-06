ef23bb2a946160552af2019fa7e9bfed22471a69
AnguiDev <angiedesarrollo@gmail.com>
2021-05-05 22:46:21 -0500

    replace item of list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return (my_list)
-    if idx > (my_list[idx]):
-        my_list[idx] = element
     my_list[idx] = element
-    return (my_list)
+    if idx > element:
+        return (my_list)
+    if idx < element:
+        return(my_list)
