63f65ed3db4a31833686f4c2d04a9212ecc8820a
AnguiDev <angiedesarrollo@gmail.com>
2021-05-05 21:15:42 -0500

    style pep8

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,5 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     """if idx exist is range"""
-    if idx in range (my_list[idx]):
+    if idx in range(my_list[idx]):
         return(my_list[idx])
