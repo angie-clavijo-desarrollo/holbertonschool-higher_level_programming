89361256060404b0408dfb57a5c03d1ff2bec93e
AnguiDev <angiedesarrollo@gmail.com>
2021-05-07 11:07:11 -0500

    new list in to copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx >= len(my_list):
-        return (my_list[element])
-    if idx < 0:
-        return (my_list[:])
+    temp = my_list[:]
+    if idx >= len(temp) or idx < 0:
+        return (temp)
+    temp[idx] = element
+    return temp
