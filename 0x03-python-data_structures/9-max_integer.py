3e7a4400029ab2df2126c44b731216e5aa1daad9
AnguiDev <angiedesarrollo@gmail.com>
2021-05-09 09:29:24 -0500

    mas value of list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    return(max(my_list[:]))
+    for i in my_list:
+        if my_list:
+            return(max(my_list[:]))
+        if my_list == 0:
+            return None
