78a1450aa5b027b68f4182705e293f32dd7f5e1d
AnguiDev <angiedesarrollo@gmail.com>
2021-05-09 17:37:48 -0500

    length of list and first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if (len(sentence) > 0):
         if sentence:
             return(len(sentence), (sentence[0]))
-    else:
+    elif (len(sentence) <= 0):
         return None
