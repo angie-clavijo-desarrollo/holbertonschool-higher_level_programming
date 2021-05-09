e06e1e733d03037bdc0576e7bbf0cf0bd906e177
AnguiDev <angiedesarrollo@gmail.com>
2021-05-09 08:48:55 -0500

    return len of tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if (len(sentence)):
+        if sentence == 0:
+            return None
         if sentence:
             return(len(sentence), (sentence[0]))
-        else:
-            return None
