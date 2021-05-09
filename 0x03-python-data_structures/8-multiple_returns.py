e6cb83eb66d836c1e004b4a1e8c2fa8e96546899
AnguiDev <angiedesarrollo@gmail.com>
2021-05-09 17:56:58 -0500

    return multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if (len(sentence)):
-        if sentence:
-            return(len(sentence), (sentence[0]))
-    else:
-        sentence[0] = None
-        return None
+    size = (len(sentence))
+    if (sentence):
+        first_letter = sentence[0]
+        return(size, first_letter)
+    return(size, None)
