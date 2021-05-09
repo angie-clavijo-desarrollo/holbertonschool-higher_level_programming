0e5dc8dcec9c82c1efdee9f17c8664aa179842f0
AnguiDev <angiedesarrollo@gmail.com>
2021-05-09 17:18:33 -0500

    max number of list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    for i in my_list:
-        if my_list:
-            return(max(my_list[:]))
-        if my_list == 0 or my_list < 0:
-            return None
+    if my_list:
+        max_num = my_list[0]
+        for i in my_list:
+            if i > max_num:
+                max_num = i
+        return(max_num)
+    return None
