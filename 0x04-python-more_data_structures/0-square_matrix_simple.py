1ba5a5973cab49c1602f3c397456a38838718630
AnguiDev <angiedesarrollo@gmail.com>
2021-05-09 22:00:50 -0500

    square of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix = [(i+1)**2 for i in range(9)]
-    for i in [matrix]:
-        return [matrix]
+    return [[x[i]**2 for i in range(len(x))]for x in matrix]
