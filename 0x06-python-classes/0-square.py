c85bd16ea563cc89b824f9e0f8eeb4b2f285acab
AnguiDev <angiedesarrollo@gmail.com>
2021-05-18 15:51:55 -0500

    square empty

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 class Square():
     def function(self):
-        """This function is for doing square
-        empty
-        """
+        """This function is for doing square empty"""
         pass
