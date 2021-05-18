4e0d2c2627fde71c8705bd7880d39c673cb229b9
AnguiDev <angiedesarrollo@gmail.com>
2021-05-17 21:56:28 -0500

    print integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value > 0 or value < 0:
-            print("{:d}".format(value))
-            return(True)
+        print("{:d}".format(value))
+        return(True)
     except ValueError:
-        if value != 0:
-            return(False)
+        return(False)
     except TypeError:
-        if value != 0:
-            return(False)
+        return(False)
