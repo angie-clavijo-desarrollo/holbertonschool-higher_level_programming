dbf5b5aa4f520961a8483ddabdd187044f3cd9d4
AnguiDev <angiedesarrollo@gmail.com>
2021-05-17 21:54:01 -0500

    print only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
             i += 1
-        print()
-        return x
-    except ValueError:
-        if value != 0:
-            return()
-    except TypeError:
-        if value != 0:
-            return()
+            count += 1
+        except ValueError:
+            print(end='')
+        except TypeError:
+            print(end='')
+    print()
+    return(count)
