b601a314bfd53c514ce3afc14c920dbf35655285
AnguiDev <angiedesarrollo@gmail.com>
2021-05-18 18:04:31 -0500

    square size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square():
+    def __init__(self, size=0):
+        self.__size = size
+""""    raise TypeError("size must be an integer")
+    raise ValueError("size must be >= 0")
+    """
