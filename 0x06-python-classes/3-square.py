e9d45e194edec7aba944a6ca5d386d4cac4bf24c
AnguiDev <angiedesarrollo@gmail.com>
2021-05-18 23:01:36 -0500

    square for size area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,11 +13,11 @@ class Square():
     Return size of area
     """
     def __init__(self, size=0):
+        self.__size = size
         if type(size).__name__ != 'int':
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
 
-    def area(area):
-        return(Square.area)
+    def area(self):
+        return(self.__size * self.__size)
