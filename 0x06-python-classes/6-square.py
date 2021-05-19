e8acf0a27d634ffe14221e2b1b728038aeda5592
AnguiDev <angiedesarrollo@gmail.com>
2021-05-19 12:48:39 -0500

    square coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,8 +14,9 @@ class Square():
     Value for setter
     Return area for square
     """
-    def __init__(self, size=0):
-        self.size = size
+    def __init__(self, size=0, position=(0, 0)):
+        self.__position = position
+        self.__size = size
 
     @property
     def size(self):
@@ -23,24 +24,11 @@ class Square():
 
     @size.setter
     def size(self, value):
-        if type(value).__name__ != 'int':
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
-
-    def area(self):
-        return(self.__size * self.__size)
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
-            return()
-        for i in range(self.__size):
-            print("".join(["#" for i in range(self.__size)]))
-
-    def __init__(self, size=0, position=(0, 0)):
-        self.size = size
 
     @property
     def position(self):
@@ -50,7 +38,21 @@ class Square():
     def position(self, value):
         if type(value) != tuple\
             or len(value) != 2\
-            or len(value[1]) != int\
-            or position[0] < 0\
-            or position[1] > 0:
+            or type(value[1]) != int\
+            or type(value[0]) != int\
+            or value[0] < 0\
+            or value[1] > 0:
             raise TypeError('position must be a tuple of 2 positive integers')
+
+    def area(self):
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+
+
