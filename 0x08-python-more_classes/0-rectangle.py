525738182aa86871110b4878622bf967167449fc
AnguiDev <angiedesarrollo@gmail.com>
2021-05-20 13:21:02 -0500

    class that define rectangle empty

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class Rectangle
+"""
+
+
+class Rectangle():
+    """"Rectangle empty
+    Define class Rectangle
+    That function, with self for created class
+    Pass is for say that is empty
+    """
+    def function(self):
+        pass
