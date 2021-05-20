4303c367ffae78dd3e570456602d399de44914dd
AnguiDev <angiedesarrollo@gmail.com>
2021-05-20 18:00:03 -0500

    list division of list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             print("wrong type")
         except ZeroDivisionError:
-            value = 0
+            result = 0
             print("division by 0")
         except IndexError:
             print("out of range")
