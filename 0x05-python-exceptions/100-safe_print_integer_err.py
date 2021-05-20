ee86ccc30a8dcc25741553f526b5f996e0b610ae
AnguiDev <angiedesarrollo@gmail.com>
2021-05-20 18:22:32 -0500

    function that print integer of value

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+import sys
+
+
+def safe_print_integer_err(value):
     try:
+        int(value)
         print("{:d}".format(value))
         return(True)
-    except ValueError:
+        raise
+    except ValueError as ex:
+        print("Exception:{}".format(ex), file=sys.stderr)
         return(False)
-    except TypeError:
-        return(False)
-    except NameError:
+    except:
+        print("Exception:{}".format(), file=sys.stderr)
         return(False)
