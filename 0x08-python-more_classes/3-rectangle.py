d5cdd414ef0662e6f44e5aed0f439ce129b6c41e
AnguiDev <angiedesarrollo@gmail.com>
2021-05-20 23:53:01 -0500

    print rectangle with #

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -11,6 +11,7 @@ class Rectangle:
     Method Area for know area of rectangle
     Method Perimeter for know perimeter of rectangle and if area and
         perimeter is 0 perimeter will be 0
+    Function magic __str__ for printing rectanfle with ##
     """
 
     def __init__(self, width=0, height=0):
@@ -49,3 +50,14 @@ class Rectangle:
             perimeter = 0
             return('')
         return(self.width + self.height) * 2
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return('')
+        stri = ('')
+        for inc in range(self.__height):
+            for inc_two in range(self.__width):
+                stri = stri + '#'
+            if inc != self.__height - 1:
+                stri = stri + '\n'
+        return (stri)
