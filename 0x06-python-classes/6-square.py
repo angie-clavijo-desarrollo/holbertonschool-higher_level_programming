5633347785baba094c44ee2ca071ba4a78cef01e
AnguiDev <angiedesarrollo@gmail.com>
2021-05-20 19:02:42 -0500

    square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,8 +49,9 @@ class Square():
 
     def my_print(self):
         if self.__size == 0:
-            print()
-        for i in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+            print('')
+        else:
+            for i in range(self.__position[1]):
+                print('')
+            for j in range(self.__size):
+                print("{}{}".format("_" * self.__position[0], "#" * self.__size))
