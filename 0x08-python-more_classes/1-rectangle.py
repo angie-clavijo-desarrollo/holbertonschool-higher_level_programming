ef3a8d857a0cbd9f961707531e3467213819aafe
AnguiDev <angiedesarrollo@gmail.com>
2021-05-20 21:00:58 -0500

    rectangle real definition

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Class Rectangle
+"""
+
+
+class Rectangle():
+    """
+    Define Class Rectangle
+    Import sys library for type error
+    Gets and Setters for object width and heigth
+    """
+    import sys
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height=height
+
+    @property
+    def width(self):
+        return(self._width)
+
+    @width.setter
+    def width(self, value):
+        if width != int:
+            print("width must be an integer", file=sys.stderr)
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            return(value)
+
+    @property
+    def height(self):
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        if height != int:
+            raise TypeError("height must be an integer", file=sys.stderr)
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            return(value)
