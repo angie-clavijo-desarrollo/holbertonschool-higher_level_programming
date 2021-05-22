3f1822dc4975cd0c25c1accb29d5b6f4c93838fd
AnguiDev <angiedesarrollo@gmail.com>
2021-05-22 14:44:29 -0500

    simple delete with function .pop

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,5 @@ def simple_delete(a_dictionary, key=""):
     if a_dictionary is None:
         return(None)
     else:
-        a_dictionary.pop(key)
+        a_dictionary.pop(key, None)
         return (a_dictionary)
