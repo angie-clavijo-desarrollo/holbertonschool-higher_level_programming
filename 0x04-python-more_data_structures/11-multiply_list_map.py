493e2510caa8c71b104974fd935906ba927cd6f6
AnguiDev <angiedesarrollo@gmail.com>
2021-05-22 16:44:16 -0500

    multiply list

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if my_list:
+    if my_list is None:
+        return(None)
+    else:
         new_list = list(map(lambda x: x * number, my_list))
         return new_list
-    else:
-        return(None)
