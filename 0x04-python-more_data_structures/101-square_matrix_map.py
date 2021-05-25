87d7a24a46e1e74651f9130b30245139a69bf533
AnguiDev <angiedesarrollo@gmail.com>
2021-05-25 15:26:21 -0500

    function that compute value square

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda count1: list(map(lambda count2: count2**2, count1)), matrix))
+    return list(map(lambda count1: list(map
+                    (lambda count2: count2**2, count1)), matrix))
