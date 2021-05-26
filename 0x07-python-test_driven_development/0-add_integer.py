cb72bda0129819336320727a5c2b0de05b88c9cc
AnguiDev <angiedesarrollo@gmail.com>
2021-05-26 10:21:25 -0500

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if type(a) != int and type(a) != float or type(a):
+    if type(a) != int and type(a) != float or type(a) == None:
         raise TypeError('a must be a integer')
 
-    if type(b) != int and type(b) != float or type(b):
+    if type(b) != int and type(b) != float or type(b) == None:
         raise TypeError('b must be a integer')
 
     return(int(a+b))
