d08349851b42517650cd5a6f83991a55c98e9e2d
AnguiDev <angiedesarrollo@gmail.com>
2021-05-31 18:46:05 -0500

    read line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open("my_file_0.txt", mode="r", encoding="utf-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         lines = f.read()
         print(lines)
     f.closed
