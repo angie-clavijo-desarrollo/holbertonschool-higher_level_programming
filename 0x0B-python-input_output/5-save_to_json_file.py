27b8b4eeee7a5c827cd2d555f56aef644a8dba24
AnguiDev <angiedesarrollo@gmail.com>
2021-05-31 22:58:34 -0500

    save to json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function save_to_json_file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Function that write the file
+    And mode write, encoding utf-8
+    Save of datas of file extern
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        output = f.write(str(my_obj))
+        return json.dumps(my_obj)
