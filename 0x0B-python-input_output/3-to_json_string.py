0313cea9d9b7e1a4ddd57259657e8c97ef51d14d
AnguiDev <angiedesarrollo@gmail.com>
2021-05-31 22:01:55 -0500

    file .json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,8 +7,6 @@ import json
 def to_json_string(my_obj):
     """
     Function that return the file.json
-    And mode write, encoding utf-8
-    Print return string of file extern
+    return string of file extern
     """
-    with open('my_obj', mode="w", encoding="utf-8") as f:
-        return json.dumps(my_obj)
+    return json.dumps(my_obj)
