437548ce572bd900ebbe53c67e215e3dc1724c56
AnguiDev <angiedesarrollo@gmail.com>
2021-05-31 19:16:47 -0500

    read line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Function read_file
+"""
+
+
 def read_file(filename=""):
+    """
+    Function that read other file
+    As with that open file
+    And mode read, encoding utf-8
+    Print lines of file extern
+    """
     with open(filename, mode="r", encoding="utf-8") as f:
         lines = f.read()
         print(lines, end="")
