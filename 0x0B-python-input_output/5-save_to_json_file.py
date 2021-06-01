bc98b9de9c8055c2b6460e32b485bcd7b2046c6e
AnguiDev <angiedesarrollo@gmail.com>
2021-05-31 23:00:56 -0500

    save to json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,4 +14,4 @@ def save_to_json_file(my_obj, filename):
     """
     with open(filename, mode="w", encoding="utf-8") as f:
         output = f.write(str(my_obj))
-        return json.dumps(my_obj)
+        return json.dumps(output)
