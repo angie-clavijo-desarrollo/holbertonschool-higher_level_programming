9cb159962f6beb4754a0b29b20b029dc937a1183
AnguiDev <angiedesarrollo@gmail.com>
2021-06-01 23:06:00 -0500

    remplace parameters of student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """
+    Def instance atrributes of class student
+    Instances publics of attributes first_name, last_name, age
+    self espicify  methods and atributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (attrs is None):
+            return(self.__dict__)
+
+        attrs_all = {}
+
+        for i in range(len(attrs)):
+            if attrs[i] in self.__dict__:
+                aux = {
+                    attrs[i]: getattr(self, attrs[i])
+                }
+                attrs_all.update(aux)
+        return attrs_all
+
+    def reload_from_json(self, json):
+        return(self.__dict__.update(json))
