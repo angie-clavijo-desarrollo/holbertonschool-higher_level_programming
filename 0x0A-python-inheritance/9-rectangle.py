703a0727f146f11f9612ebc1ff0f2d023eb8d9de
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-06-03 17:59:18 -0500

    area by rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This function import file 7 base geometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle
+    That verify the area of rectangle
+    in base width and height
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("width", width)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+if __name__ == "__main__":
+    r = Rectangle(3, 5)
+
+    print(r)
+    print(r.area())
