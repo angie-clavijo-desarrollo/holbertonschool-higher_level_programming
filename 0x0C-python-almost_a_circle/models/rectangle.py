2e56cdddd86374cbf0b5dc7a924806d077372341
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-06-05 22:35:34 -0500

    corrections

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """created class that inherits of Base"""
-Base = __import__('base').Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -9,88 +9,83 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """call super class, with id, call with __init__ ,  or class parent"""
         super().__init__(id)
-        """private instance atributes"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        """validations"""
+
         if type(width) is not int:
             raise TypeError('width must be an integer')
         if width <= 0:
             raise ValueError('width must be > 0')
 
-        """validations"""
         if type(height) is not int:
             raise TypeError('height must be an integer')
         if height <= 0:
             raise ValueError('height must be > 0')
 
-        """validations"""
         if type(x) is not int:
             raise TypeError('x must be an integer')
         if x < 0:
             raise ValueError('x must be >= 0')
 
-        """validations"""
         if type(y) is not int:
             raise TypeError('y must be an integer')
         if y < 0:
             raise ValueError('y must be >= 0')
 
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-        """getter and setter for each instance attributes"""
-        @property
-        def width(self):
-            return self.__width
+    """getter and setter for each instance attributes"""
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, width):
-            """validations"""
-            if type(width) is not int:
-                raise TypeError('width must be an integer')
-            if width <= 0:
-                raise ValueError('width must be > 0')
-            self.__width = width
+    @width.setter
+    def width(self, width):
+        self.__width = width
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-        @height.setter
-        def height(self, height):
-            """validations"""
-            if type(height) is not int:
-                raise TypeError('height must be an integer')
-            if height <= 0:
-                raise ValueError('height must be > 0')
-            self.__height = height
+    @height.setter
+    def height(self, height):
+        self.__height = height
 
-        @property
-        def x(self):
-            return self.__x
+    @property
+    def x(self):
+        return self.__x
 
-        @x.setter
-        def x(self, x):
-            """validations"""
-            if type(x) is not int:
-                raise TypeError('x must be an integer')
-            if x < 0:
-                raise ValueError('x must be >= 0')
-            self.__x = x
+    @x.setter
+    def x(self, x):
+        self.__x = x
 
-        @property
-        def y(self):
-            return self.__y
+    @property
+    def y(self):
+        return self.__y
 
-        @y.setter
-        def y(self, y):
-            """validations"""
-            if type(y) is not int:
-                raise TypeError('y must be an integer')
-            if y < 0:
-                raise ValueError('y must be >= 0')
-            self.__y = y
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+    """update class Rectangle, add public method, return area value"""
+    def area(self):
+        return self.__width * self.__height
+
+    """
+    update class Rectangle, add public method,
+    print # for width and height
+    """
+    def display(self):
+        if self.__width == 0 or self.__height:
+            return ('')
+        str = ('')
+        for iter_one in range(self.__height):
+            for iter_two in range(self.__width):
+                str = str + '#'
+            if iter_one != self.__height - 1:
+                str = str + '\n'
+            return str
 
 
 if __name__ == "__main__":
@@ -116,5 +111,3 @@ if __name__ == "__main__":
         Rectangle(10, 2, 3, -1)
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
-
-
