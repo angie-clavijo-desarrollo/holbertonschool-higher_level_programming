d28a7b31d0e5d405ab26136da0c239c6c9bfe077
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-06-08 23:44:16 -0500

    comment of functions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ from base import Base
 
 
 class Rectangle(Base):
+    """Class new that inhegitids of Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor, instance attributes
