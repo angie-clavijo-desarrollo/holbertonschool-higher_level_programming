268e53ee35139bd7e1a4260d321289ec84c3fc39
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-06-08 23:51:25 -0500

    private funct

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -129,3 +129,15 @@ class Rectangle(Base):
         for i in range(len(vars)):
             dict.update({vars[i]: getattr(self, vars[i])})
         return dict
+
+if __name__ == "__main__":
+    from rectangle import Rectangle
+
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
