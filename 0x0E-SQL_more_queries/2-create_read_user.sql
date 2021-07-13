10d0b57cec37665465dd393d0c5efa2bb5b871c0
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-07-13 16:05:28 -0500

    created the database hbtn_0d_2 and user and privelege SELECT

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS
     'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+USE hbtn_0d_2;
 GRANT SELECT
     ON *.*
     TO 'user_0d_2'@'localhost';
