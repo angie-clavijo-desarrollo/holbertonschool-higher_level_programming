d8011e16a5e748d76a5ffc7c63988f51d8424cdb
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-07-13 23:22:44 -0500

    database dump from hbtn_0d_tvshows to your MySQL server

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-WHERE tv_shows.id = tv_show_genres_id
+WHERE tv_shows.id = tv_show_genres_id.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
