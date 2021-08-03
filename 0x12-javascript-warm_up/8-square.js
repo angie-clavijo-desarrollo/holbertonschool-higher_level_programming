8b1206b2336e058fa2bf59745919441fe6469b76
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-02 22:32:23 -0500

    JS print square  with double loop

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const xtime = parseInt(process.argv[2]);
+let i = 0;
+let j = 0;
+let str = '';
+
+if (xtime) {
+  while (i < xtime) {
+    while (j < xtime) {
+      str += 'x';
+      j++;
+    }
+    console.log(str);
+    j = 0;
+    str = '';
+    i++;
+  }
+} else {
+  console.log('Missing size');
+}
