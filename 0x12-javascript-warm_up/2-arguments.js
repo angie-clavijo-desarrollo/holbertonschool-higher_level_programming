07ef22d9e418926f6a7e1a1a4e474ac7ebcdce6f
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-02 19:05:21 -0500

    JS when passed arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let i = 0;
+
+while (process.argv[i]) {
+  i++;
+}
+
+if (i < 3) {
+  console.log('No argument');
+} else if (i === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
