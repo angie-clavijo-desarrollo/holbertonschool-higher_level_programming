985a07776c4fcd6603bf4bc4bfb4738cd4e32df9
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-03 16:36:15 -0500

    JS console.log rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,27 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.heigth = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let j = 0;
+    let str = '';
+
+    while (i < this.height) {
+      while (j < this.width) {
+        str += 'X';
+        j++;
+      }
+      console.log(str);
+      str = '';
+      j = 0;
+      i++;
+    }
   }
 }
 module.exports = Rectangle;
