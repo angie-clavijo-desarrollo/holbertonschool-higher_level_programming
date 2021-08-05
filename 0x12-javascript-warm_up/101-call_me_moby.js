aa5ec035d00467c9cdb58bbc6d147e406693b816
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-04 22:12:09 -0500

    JS funvtion that repeat x times ...

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i <= x; i++) {
-    return theFunction;
+exports.callMeMoby = function (x, thefunction) {
+  for (let i = 0; i < x; i++) {
+    thefunction();
   }
 };
