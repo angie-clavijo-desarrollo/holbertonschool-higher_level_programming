d20ec1d4f7cc8dda562916ddbc6b3ec27cfd6e37
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-07 17:01:06 -0500

    PY and MYSQLDB

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,22 +7,22 @@ import MySQLdb
 from sys import argv
 
 
-"""expression not execute when load"""
+# expression not execute when load
 if __name__ == "__main__":
 
-    """to connect"""
+    # to connect
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    """prepare cursor object cursor () method"""
+    # prepare cursor object cursor () method
     cursor = db.cursor()
 
-    """Execute sql query  using execute method"""
+    # Execute sql query  using execute method
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     result = cursor.fetchall()
     for row in result:
         print(row)
 
-    """disconect from server"""
+    # disconect from server
     cursor.close()
     db.close()
