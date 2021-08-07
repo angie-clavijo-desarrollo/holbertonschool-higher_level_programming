9eea4acb13d08c131a6244ee7d076f200d971c7b
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-07 16:27:26 -0500

    PY and SQL files initial

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+from sys import argv
+"""Module for conneting to MYSQL datbase"""
+
+"""expression not execute when load"""
+if __name__ == "__main__":
+
+    """to connect"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    """prepare cursor object cursor () method"""
+    cursor = db.cursor()
+
+    """Execute sql query  using execute method"""
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE  states.name LIKE 'N%'")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    """disconect from server"""
+    cursor.close()
+    db.close()
