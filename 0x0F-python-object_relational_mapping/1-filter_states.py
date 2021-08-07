6371de41135e46a2a3bc06317968b430e01dbda7
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-07 18:08:48 -0500

    PY and MySQLdb and doing query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-import sys
+"""
+Module for conneting to MYSQLdb datbase
+and view the arguments of the database
+and doing query at database
+"""
 import MySQLdb
 from sys import argv
-"""Module for conneting to MYSQL datbase"""
 
-"""expression not execute when load"""
+# expression not execute when load"""
 if __name__ == "__main__":
 
-    """to connect"""
+    # to connect
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    """prepare cursor object cursor () method"""
+    # prepare cursor object cursor () method
     cursor = db.cursor()
 
-    """Execute sql query  using execute method"""
+    # Execute sql query  using execute method
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     cursor.execute("SELECT * FROM states WHERE  states.name LIKE 'N%'")
     result = cursor.fetchall()
     for row in result:
         print(row)
 
-    """disconect from server"""
+    # disconect from server
     cursor.close()
     db.close()
