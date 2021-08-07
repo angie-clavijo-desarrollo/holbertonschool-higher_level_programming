a3950cb4c8e6bd1056eced1aa7df33252e11517d
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-07 18:50:44 -0500

    PY and MySQLdb and doing query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-import sys
 import MySQLdb
 from sys import argv
-"""Module for conneting to MYSQL datbase"""
 
-"""expression not execute when load"""
+
+# expression not execute when load
 if __name__ == "__main__":
 
-    """to connect"""
+    # to connect
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    """prepare cursor object cursor () method"""
+    # prepare cursor object cursor () method
     cursor = db.cursor()
 
-    """Execute sql query  using execute method"""
+    # Execute sql query  using execute method
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 #    cursor.execute("SELECT * FROM states WHERE  states.id LIKE '2%'")#
     cursor.execute("SELECT * FROM states WHERE name='Arizona'")
@@ -22,6 +21,5 @@ if __name__ == "__main__":
     for row in result:
         print(row)
 
-    """disconect from server"""
-    cursor.close()
+    # disconect from server
     db.close()
