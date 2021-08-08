01f07eaf8ad4984ecb97ce708af289b32724710b
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-08 17:59:24 -0500

    PY and SQLalquemy created table and connect with database

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-
+Module that connect a database
+with port 3306 and create table
+as syntax of SQLalchemy
 """
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
