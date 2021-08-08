91538e798f6d3245fe6b989274dd0009bea2675b
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-07 23:57:16 -0500

    PY and MYSQLdb query INNERJOIN

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for connecting to MYSQLdb database
+and view the arguments of the database
+and doing query at database
+"""
+import MySQLdb
+from sys import argv
+
+
+# expression not execute when load
+if __name__ == "__main__":
+
+    # to connect
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # prepare cursor object cursor () method
+    cursor = db.cursor()
+
+    # Execute sql query  using execute method
+    cursor.execute("   SELECT cities.name FROM cities\
+                    INNER JOIN states ON cities.state_id = states.id\
+                    WHERE states.name = %s;", [argv[4]])
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    # disconnect from server
+    cursor.close()
+    db.close()
