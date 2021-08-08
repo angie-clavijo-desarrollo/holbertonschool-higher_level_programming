1ac1c46b0e15ce0281ffccab7d50d960bdb845db
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-08 17:47:09 -0500

    PY and SQLalchemy created conection

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# connect database
+# engine = create_engine("mysql://root:root@localhost:3306/db")
+
+# super class that permit of mapping of class children
+Base = declarative_base()
+
+
+# inherits
+class State(Base):
+    __tablename__ = 'states'
+    # attributes
+    id = Column(Integer, primary_key=False)
+    name = Column(String(128), nullable=False)
+
+# connect and created table
+# Base.metadata.create_all(engine)
