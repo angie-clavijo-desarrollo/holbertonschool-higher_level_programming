f519dfb9e964895713239eefd9a5472652ef9efc
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-08 21:54:29 -0500

    PY and SQLalquemy created table and connect with database

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for connecting to SQLalquemy database
+and view the arguments of the database
+and doing query at database
+"""
+from sys import argv
+from sqlalchemy import create_engine, delete
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+# expression not execute when load
+if __name__ == "__main__":
+
+    # to connect
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # configuration session
+    Session = sessionmaker(bind=engine)
+
+    # created session with constructor:
+    session = Session()
+
+    Base.metadata.create_all(engine)
+
+    x = delete(State).where(State.name.contains('%a%'))
+
+    session = session.close()
