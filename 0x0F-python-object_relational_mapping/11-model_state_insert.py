791949536bc3e0e050172d90d1395a6af22d7e74
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-08 20:56:02 -0500

    PY and SQLalquemy created table and connect with database

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -28,7 +28,8 @@ if __name__ == "__main__":
 
     c1 = State(name="Louisiana")
     session.add(c1)
-    print(c1)
+
+    print(session.query(State).filter_by(name="Louisiana").first().id)
     session.commit()
 
     session = session.close()
