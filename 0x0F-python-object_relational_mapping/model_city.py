685c94dac8f95dd3f4059e8560076651f154aae1
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-08 23:04:52 -0500

    PY and SQLalquemy created table and connect with database

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,7 +10,7 @@ from model_state import Base
 
 class City(Base):
     """ inherits"""
-    __tablename__ = 'city'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False, )
