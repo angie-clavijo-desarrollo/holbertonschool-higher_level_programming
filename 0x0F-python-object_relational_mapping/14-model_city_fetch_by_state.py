01de63fe82cff0162748e63aad8fb54e74be61f6
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-08 23:01:18 -0500

    PY and SQLalquemy created table and connect with database

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,21 +10,14 @@ from model_city import City
 from model_state import Base, State
 from sys import argv
 
-
 if __name__ == "__main__":
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-
     session = Session()
-
     Base.metadata.create_all(engine)
 
     city = session.query(State, City).join(City).order_by(City.id)
     for state, city in city:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-
-    session = session.close()
+    session.close()
