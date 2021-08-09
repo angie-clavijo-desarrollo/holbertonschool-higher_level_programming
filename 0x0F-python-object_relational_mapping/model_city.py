3344a01b89ddb55897f51fe289fe87d85f1ddfa4
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-08 22:08:40 -0500

    PY and SQLalquemy created table and connect with database

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module that connect a database
+with port 3306 and create table
+as syntax of SQLalchemy
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+# super class that permit of mapping of class children
+
+
+class City(Base):
+    """ inherits"""
+    __tablename__ = 'city'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
