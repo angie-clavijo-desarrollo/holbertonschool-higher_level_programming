2d125ff963ee2f87c2881310a8d3b2a8db6ef803
angiedesarrollo@gmail.com <angiedesarrollo@gmail.com>
2021-08-20 13:30:32 -0500

    URLLIB fetches url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 # script that fetches url
 import urllib.request
+
+
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print('-', html)
+    types = type(html)
+    print("Body response:\n",
+          "\t- type: {}\n".format(types),
+          "\t- content: {}\n".format(html),
+          "\t- utf8 content: {}".format(html.decode()))
