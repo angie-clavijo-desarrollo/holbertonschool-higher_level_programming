8924d0183609927549b00d2596f747f30d910408
angiedesarrollo@gmail.com <angiedesarrollo@gmail.com>
2021-08-20 14:54:51 -0500

    URLLIB get header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response"""
+import urllib.request
+import sys
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    html = response.read()
+    print(response.getheader("X-Request-Id"))
