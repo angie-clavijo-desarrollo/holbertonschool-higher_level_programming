b1ad52ce8ba4b6e6b5c0f6421ede1718117a810a
angiedesarrollo@gmail.com <angiedesarrollo@gmail.com>
2021-08-20 23:01:07 -0500

    REQUEST json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,15 +4,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = ('http://880d559bbb2f.4fd667c2.hbtn-cod.io:5000/search_user')
+    url = ('http://0.0.0.0:5000/search_user')
 
     if (len(sys.argv) > 1):
         data = {'q': sys.argv[1]}
     else:
         data = {'q': ""}
 
-    req = requests.post(url, data)
-    
+    req = requests.post(url, data) 
     try:
         if len(r.json()) == 0:
             print("No result")
