6fc0ffd64946d8c4376b27a5c2268d3831757253
angiedesarrollo@gmail.com <angiedesarrollo@gmail.com>
2021-08-20 21:35:05 -0500

    REQUEST error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     req = requests.get(url)
 
     if req.status_code < 400:
-        print(req)
+        print(req.text)
     else:
         print("Error code: {}".format(req.status_code))
