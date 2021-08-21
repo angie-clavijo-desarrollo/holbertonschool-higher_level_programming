c8c2856404326c9b53949d60a1ec84497286412b
angiedesarrollo@gmail.com <angiedesarrollo@gmail.com>
2021-08-20 23:27:11 -0500

    REQUEST json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,18 +4,25 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = ('http://0.0.0.0:5000/search_user')
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {}
 
-    if (len(sys.argv) > 1):
+    if len(sys.argv) > 1:
         data = {'q': sys.argv[1]}
+        print("5")
     else:
         data = {'q': ""}
+        print("4")
 
-    req = requests.post(url, data) 
+    req = requests.post(url, data=data)
     try:
-        if len(r.json()) == 0:
+        size = req.json()
+        if size == {}:
             print("No result")
+            print("3")
         else:
-            print("[{}] {}".format(r.json().get('id'), r.json.get('name')))
-    except:
+            print("[{}] {}".format(size["id"], size["name"]))
+            print("2")
+    except ValueError:
         print("Not a valid JSON")
+        print("1")
