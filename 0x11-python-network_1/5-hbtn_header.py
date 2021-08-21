a3f5f304fb5832b23538d636a2fcf43b14c47840
angiedesarrollo@gmail.com <angiedesarrollo@gmail.com>
2021-08-20 19:29:59 -0500

    URLLIB send request header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" """
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+        req =  urllib.request.urlopen(sys.argv[1])
+        print(req.getheader("X-Request-Id"))
