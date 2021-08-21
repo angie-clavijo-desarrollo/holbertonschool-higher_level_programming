a2932a3d9a97b9f93106e7839382f1f19e4fbad6
angiedesarrollo@gmail.com <angiedesarrollo@gmail.com>
2021-08-20 19:09:25 -0500

    URLLIB status code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
+        htm = ''
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
+        print("\t- type: {}".format(type(htm)))
         print("\t- content: {}".format(html.decode()))
