ea8bf64553a552f8b8e1723b64d1ce328cd0eb08
angiedesarrollo@gmail.com <angiedesarrollo@gmail.com>
2021-08-20 21:30:38 -0500

    REQUEST error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Error code"""
-from requests.exceptions import HTTPError, Timeout
 import requests
 import sys
 
 if __name__ == "__main__":
-    try:
-        url = sys.argv[1]
-        req = requests.get(url)
+    url = sys.argv[1]
+    req = requests.get(url)
+
+    if req.status_code < 400:
         print(req)
-    except HTTPError as error:
-        print(f'Http Error: {error}')
+    else:
+        print("Error code: {}".format(req.status_code))
